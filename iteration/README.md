70d32b92f9533ab0b741d15e36bb746405b12deb
Taylor Varoglu <tava8398@colorado.edu>
2021-04-20 11:30:22 -0600

    Add completed count_pattern_spec to iteration/exercises

--- iteration/README.md
+++ iteration/README.md
@@ -7,7 +7,7 @@ Suggested Order:
 * [x] map_pattern_test.rb
 * [x] find_pattern_test.rb
 * [x] select_pattern_test.rb
-* [ ] count_pattern_test.rb
+* [x] count_pattern_test.rb
 * [ ] all_pattern_test.rb
 * [ ] max_and_min_by_pattern_test.rb
 * [ ] inject_pattern_test.rb
--- iteration/exercises/spec/count_pattern_spec.rb
+++ iteration/exercises/spec/count_pattern_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe 'count pattern' do
         younger_than_thirty += 1
       end
     end
+
     expect(younger_than_thirty).to eq(2)
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     ages = {
       abdi: 39,
       hassan: 45,
@@ -25,20 +26,23 @@ RSpec.describe 'count pattern' do
         younger_than_thirty += 1
       end
     end
+
     expect(younger_than_thirty).to eq(2)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     ages = [39, 45, 29, 24, 50]
     older_than_fifty = 0
     ages.each do |age|
-      # Your Code Here
+      if age > 50
+        older_than_fifty += 1
+      end
     end
 
     expect(older_than_fifty).to eq(0)
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     ages = {
       abdi: 39,
       hassan: 45,
@@ -48,21 +52,27 @@ RSpec.describe 'count pattern' do
     }
     older_than_fifty = 0
     ages.each do |name, age|
-      # Your Code Here
+      if age > 50
+        older_than_fifty += 1
+      end
     end
 
     expect(older_than_fifty).to eq(0)
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     ages = [39, 45, 29, 24, 50]
     multiple_of_three = 0
-    # Your Code Here
+    ages.each do |age|
+      if age % 3 == 0
+        multiple_of_three += 1
+      end
+    end
 
     expect(multiple_of_three).to eq(3)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     ages = {
       abdi: 39,
       hassan: 45,
@@ -71,20 +81,28 @@ RSpec.describe 'count pattern' do
       miguel: 50
     }
     multiple_of_three = 0
-    # Your Code Here
+    ages.each do |person, age|
+      if age % 3 == 0
+        multiple_of_three += 1
+      end
+    end
 
     expect(multiple_of_three).to eq(3)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     family = ["alice", "bob", "charlie", "david", "eve"]
-    # Your Code Here
+    names_with_three_letters = 0
+    family.each do |person|
+      if person.length == 3
+        names_with_three_letters += 1
+      end
+    end
 
-    assert_equal 2, names_with_three_letters
     expect(names_with_three_letters).to eq(2)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     family = {
       mother: "alice",
       father: "bob",
@@ -92,21 +110,29 @@ RSpec.describe 'count pattern' do
       uncle: "david",
       sister: "eve"
     }
-    # Your Code Here
+    names_with_three_letters = 0
+    family.each do |member, name|
+      if name.length == 3
+        names_with_three_letters += 1
+      end
+    end
 
-    assert_equal 2, names_with_three_letters
     expect(names_with_three_letters).to eq(2)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     prices = [1.0, 3.9, 5.99, 18.5, 20.0]
-    # Your code goes here
+    whole_numbers = 0
+    prices.each do |price|
+      if price.to_s[-1].to_i == 0
+        whole_numbers += 1
+      end
+    end
 
-    assert_equal 2, whole_numbers
     expect(whole_numbers).to eq(2)
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     prices = {
       "shoes" => 1.0,
       "backpack" => 3.9,
@@ -114,7 +140,12 @@ RSpec.describe 'count pattern' do
       "posters" => 18.5,
       "food" => 20.0
     }
-    # Your Code Here
+    whole_numbers = 0
+    prices.each do |item, price|
+      if price.to_s[-1].to_i == 0
+        whole_numbers += 1
+      end
+    end
 
     expect(whole_numbers).to eq(2)
   end
